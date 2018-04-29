198ebe6dfa9c3975a4ef3c39d1b36562cad3f278
Yinka Ash <developer@designsbynetguru.com>
2018-04-30 01:02:13 +0800

    Created Contact Form Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :contacts, only: [:new, :create]
   get '/about' => 'pages#about'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
