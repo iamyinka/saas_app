d79bcb306fc9a4bbbf7f9f33384e228b64278a1b
Yinka Ash <developer@designsbynetguru.com>
2018-04-30 13:05:41 +0800

    Added Validations & Create Action for Contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,23 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      flash[:success] = "Message Successfully Sent"
+      redirect_to contact_us_path
+    else
+      flash[:danger] = "Error Occurred! Please ensure all fields are correctly filled"
+      redirect_to contact_us_path
+    end
     
   end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :comments)
+  end
+  
 
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ActiveRecord::Base
 
-  
+  validates :name, presence: true, length: {minimum:3, maximum:100}
+  validates :email, presence: true, length: {minimum:6, maximum:150}
   
 end
