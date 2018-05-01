80bd85f9ea8d0e4f1ac26a5389fb409225ac46ac
Yinka Ash <developer@designsbynetguru.com>
2018-05-02 03:08:40 +0800

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   get '/contact_us' => 'contacts#new'
   resources :contacts, only: [:new, :create]
   get '/about' => 'pages#about'
