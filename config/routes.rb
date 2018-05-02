2e6b5a1b03ad1e672f2b52a6dc80d9569e724f80
Yinka Ash <developer@designsbynetguru.com>
2018-05-03 07:28:52 +0800

    Added Profile Controller & Nested Routes for User Profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  resources :users do
+    resource :profile
+  end
   get '/contact_us' => 'contacts#new'
   resources :contacts, only: [:new, :create]
   get '/about' => 'pages#about'
